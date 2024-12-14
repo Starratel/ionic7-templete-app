682db0a27fb38b565d0f52e8f56313d8d0d3f99d
KenzeevAndrei <kenzeev04@list.ru>
2024-12-14 18:23:37 +0300

    fill:add gaga

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -1,14 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Drink{
+  name:string
+  year:number
+  owner:string
+}
+
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.component.html',
   styleUrls: ['./intro.component.scss'],
 })
 export class IntroComponent  implements OnInit {
+abc:String="ayk"
+my_list: string[] = ["sdgsdgf", "fsgdgfgfgd"]
 
-  constructor() { }
+  drinks: Drink[] = [
+  {
+    name: "Мот",
+    year: 19655,
+    owner: "France"
+  },{
+    name: "Мот",
+    year: 19655,
+    owner: "France"
+  },{
+    name: "Мот",
+    year: 19655,
+    owner: "France"
+  }
+]
 
+  constructor() { 
+
+  }
   ngOnInit() {}
   
 
